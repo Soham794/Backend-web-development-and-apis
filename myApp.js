8b87e25e9720913dde64f5f0efa3a11edc4d230a
Soham Belokar <soham.belokar794@gmail.com>
2025-07-04 14:30:24 +0530

    custom middleware

--- myApp.js
+++ myApp.js
@@ -6,13 +6,23 @@ let app = express();
 
 app.use("/public", express.static(__dirname+"/public"));
 
+// custom middleware function
+function midWare(req, res, next){
+    console.log(req.method + " " + req.path + " " + req.ip);
+    next();
+}
+
+// using custom middleware on every request
+// for specific use -> app.post(<midware-function>);
+app.use(midWare);
+
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/json", (req, res)=>{
     // let msg = "Hello json";
-    console.log(process.env.MESSAGE_STYLE);
+    // console.log(process.env.MESSAGE_STYLE);
     if(process.env.MESSAGE_STYLE === "uppercase"){
         // msg = msg.toUpperCase();
         res.json({"message": "HELLO JSON"});
